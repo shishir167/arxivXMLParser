120733a672beec88dd3ee460dd5297cb048bc191
shishir <sk3432@drexel.edu>
2016-03-02 16:02:04 -0500

    arxiv api call setup

--- arxiv.py
+++ arxiv.py
@@ -4,13 +4,15 @@ import time
 
 
 def main():
-
-    xmlResult = callArxivApi("?verb=ListRecords&set=cs&metadataPrefix=arXiv");
+    xmlResult = callArxivApi("?verb=ListRecords&set=cs&metadataPrefix=arXiv")
+    print(len(xmlResult))
     token = getResumptionToken(xmlResult)
-    print ("token = ", token)
-    newPath = "?verb=ListRecords&resumptionToken=" + token
-    print (newPath)
-    callArxivApi(newPath)
+    for x in range(0, 9):
+        print ("x =", x)
+        print ("token = ", token)
+        newPath = "?verb=ListRecords&resumptionToken=" + token
+        xmlResult = callArxivApi(newPath)
+        token = getResumptionToken(xmlResult)
 
 def getResumptionToken(parseString):
     print ("Getting resumptionToken")
@@ -23,21 +25,26 @@ def callArxivApi(path):
     url = url + path
     print (url)
     s = ""
-
     try:
         with urllib.request.urlopen(url) as url:
             s = url.read()
+            print ("returning xml")
+            return s
     except urllib.error.HTTPError as e:
         print("got Http error")
         print("trying after 20 seconds")
         time.sleep(20)
-        callArxivApi(path)
+        return callArxivApi(path)
+    except urllib.error.URLError as e:
+        print("got URL error", str(e))
+        print("trying after 20 seconds")
+        time.sleep(2)
+        return callArxivApi(path)
     except urllib.error.ContentTooShortError as e:
         print("got Content too short error")
         print("trying after 2 seconds")
-        time.sleep(2)
-        callArxivApi(path)
-    return s
+        time.sleep(20)
+        return callArxivApi(path)
 
 
 if __name__ == "__main__":
